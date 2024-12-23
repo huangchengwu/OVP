1e115959f65dec3fa572d054e870bf5735e7d006
huangchengwu <751164212@qq.com>
2024-12-23 17:46:43 +0800

    add

--- start.sh
+++ start.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-make run 
+Dir="/OVPs/"
+
+if [ -d "/data/" ]; then
+    echo "持久化启动"
+    if [ ! -f "/data/db.sqlite3" ]; then
+        echo "第一次启动"
+        mv db.sqlite3 /data/db.sqlite3
+        ln -s /data/db.sqlite3 ${Dir}
+    else
+        echo "启动链接"
+        rm db.sqlite3
+        ln -s /data/db.sqlite3 ${Dir}
+    fi
+else
+    echo "无持久化启动"
+fi
+cd ${Dir}
+
+make run
